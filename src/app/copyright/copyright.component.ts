4d250e53b85bb0543cc87081a01314bfb35c2b6e
Nicole Kemp <nicole.abbott@live.co.za>
2019-08-15 07:03:51 +0200

    File clean up

--- src/app/copyright/copyright.component.ts
+++ src/app/copyright/copyright.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-copyright',
   templateUrl: './copyright.component.html',
-  styleUrls: ['./copyright.component.scss']
 })
 export class CopyrightComponent implements OnInit {
 
